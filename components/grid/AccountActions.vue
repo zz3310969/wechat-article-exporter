020f2be8499f592614c538e5abba7b87a05ba554
jock <findsource@proton.me>
2026-01-11 11:39:43 +0800

    添加复制账号链接按钮

--- components/grid/AccountActions.vue
+++ components/grid/AccountActions.vue
@@ -21,10 +21,26 @@ function stop() {
 }
 const isDisabled = computed(() => props.params.isDeleting || props.params.isSyncing);
 const isLoading = computed(() => props.params.isSyncing && props.params.node.id === props.params.syncingRowId);
+
+const copied = ref(false);
+function copyWechatLink() {
+  const link = `https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=${props.params.data.fakeid}&scene=124#wechat_redirect`;
+  navigator.clipboard.writeText(link);
+
+  copied.value = true;
+  setTimeout(() => {
+    copied.value = false;
+  }, 1000);
+}
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
+  <div class="flex items-center justify-center gap-3">
+    <UIcon v-if="copied" name="i-lucide:check" class="size-5 text-gray-500" />
+    <UTooltip v-else text="复制微信链接" :popper="{ placement: 'left' }">
+      <UButton icon="i-heroicons-link-16-solid" size="xs" color="blue" @click="copyWechatLink" />
+    </UTooltip>
+
     <UButton v-if="isLoading" color="green" size="xs" variant="solid" @click="stop">
       <Loader :size="14" class="animate-spin" />
       停止</UButton
@@ -34,7 +50,6 @@ const isLoading = computed(() => props.params.isSyncing && props.params.node.id 
       icon="i-heroicons:arrow-path-rounded-square-20-solid"
       color="blue"
       size="xs"
-      variant="solid"
       :disabled="isDisabled"
       @click="sync"
     ></UButton>
